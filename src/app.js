1b55dffff4e8e96a5833c1c5f97e628e473359b6
Pascal Zirn <zirn.pascal@gmail.com>
2021-11-01 10:34:28 +0100

    streamer list is rendering

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
-'use strict';
+"use strict";
 
 import "./stylesheets/main.css";
 import { ipcRenderer } from "electron";
 
+let streamersArray = ["LOADING..."];
+
 ipcRenderer.on("pause-status", (e, paused) => {
   console.log("---", paused);
 });
-ipcRenderer.on("streamers", (e, streamers) => {
-  console.log("---", streamers);
-});
-
-document.querySelector("#btn").addEventListener("click", (e) => {
-  e.preventDefault();
-  ipcRenderer.send("get-streamers", e.target.href);
-});
-
 
 
 
@@ -23,20 +16,42 @@ document.querySelector("#btn").addEventListener("click", (e) => {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { streamers: ["loading..."] };
+  }
+
+  componentWillMount() {
+    ipcRenderer.on("streamers", (e, streamers) => {
+      this.setState({streamers: streamers })
+    });
+  }
+
+  componentDidMount() {
+    ipcRenderer.send("get-streamers")
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+    // if (this.state.liked) {
+    //   return 'You liked this.';
+    // }
+    // <button onClick={() => this.setState({ liked: true })}></button>
+
+    function Item(props) {
+      return <li>{props.name}</li>;
     }
 
+
+
     return (
-      <button onClick={() => this.setState({ liked: true })}>
-        Like
-      </button>
+      <div>
+        <ul>
+          {this.state.streamers.map((streamer, i) => (
+            <Item key={i} name={streamer} />
+          ))}
+        </ul>
+      </div>
     );
   }
 }
+
 
 ReactDOM.render(React.createElement(App), document.querySelector("#app"));
